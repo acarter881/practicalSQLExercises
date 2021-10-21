76075e64f97dedfd91ee4a76b5ac775699c50712
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-10-21 13:30:33 -0500

    Update 11.2.sql

--- 11.2.sql
+++ 11.2.sql
@@ -1,3 +1,11 @@
+/*
+QUESTION:
+Using the AT TIME ZONE keywords, write a query that displays the
+date and time for London, Johannesburg, Moscow, and
+Melbourne the moment January 1, 2100, arrives in New York
+City.
+*/
+
 SELECT
 	test_date AT TIME ZONE 'America/New_York' AS "New York City",
 	test_date AT TIME ZONE 'Europe/London' AS "London",
